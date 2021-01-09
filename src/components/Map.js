6489508148eac3d725be751c08453f1ef3011f49
Trent Segura <trnt@me.com>
2021-01-09 11:41:38 -0700

    add lines?

--- src/components/Map.js
+++ src/components/Map.js
@@ -39,34 +39,9 @@ export class Map extends Component {
             map: map
         })
 
-        
-
-        
-
-
-
+    
 
         map.on('load', function () {
-
-            // map.addSource('mapbox-dem', {
-            //     'type': 'raster-dem',
-            //     'url': 'mapbox://mapbox.mapbox-terrain-dem-v1',
-            //     'tileSize': 512,
-            //     'maxzoom': 14
-            //     });
-            //     // add the DEM source as a terrain layer with exaggerated height
-            // map.setTerrain({ 'source': 'mapbox-dem', 'exaggeration': 1.5 });
-                    
-            //     // add a sky layer that will show when the map is highly pitched
-            // map.addLayer({
-            //     'id': 'sky',
-            //     'type': 'sky',
-            //     'paint': {
-            //     'sky-type': 'atmosphere',
-            //     'sky-atmosphere-sun': [0.0, 0.0],
-            //     'sky-atmosphere-sun-intensity': 15
-            //     }
-            //     });
 
             map.addSource('landlines', {
                 'type': 'geojson',
